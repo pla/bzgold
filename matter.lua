b5d82131bd34d04c97b6fb19676583c5f2c5648f
pla <massenpost@pla1.de>
2025-10-22 18:05:37 +0200

    check if gold to matter exits already

--- matter.lua
+++ matter.lua
@@ -1,19 +1,21 @@
 -- Matter recipes for Krastorio2
 if mods["Krastorio2"] then
-  local util = require("data-util");
+  local util = require("data-util")
 
-  util.k2matter({
-    k2matter = {
-      material = { type = "item", name = "gold-ore", amount = 30 },
-      item_name = "gold-ore",
-      matter_count = 30,
-      energy_required = 10,
-      need_stabilizer = false,
-      unlocked_by_technology = "gold-matter-processing",
-    },
-    k2baseicon = "stone",
-    icon = {icon = "__bzgold__/graphics/icons/gold-ore.png", icon_size = 128, scale = 1}
-  })
+  if not data.raw.recipe["kr-gold-ore-to-matter"] then
+    util.k2matter({
+      k2matter = {
+        material = { type = "item", name = "gold-ore", amount = 30 },
+        item_name = "gold-ore",
+        matter_count = 30,
+        energy_required = 10,
+        need_stabilizer = false,
+        unlocked_by_technology = "gold-matter-processing",
+      },
+      k2baseicon = "stone",
+      icon = { icon = "__bzgold__/graphics/icons/gold-ore.png", icon_size = 128, scale = 1 },
+    })
+  end
 
   util.k2matter({
     k2matter = {
@@ -25,6 +27,6 @@ if mods["Krastorio2"] then
       unlocked_by_technology = "silver-matter-processing",
     },
     k2baseicon = "stone",
-    icon = {icon = "__bzgold__/graphics/icons/silver-ore.png", icon_size = 128, scale = 1}
+    icon = { icon = "__bzgold__/graphics/icons/silver-ore.png", icon_size = 128, scale = 1 },
   })
 end
